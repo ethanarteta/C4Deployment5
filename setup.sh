90df87b43e015228c41cd0ee17e98dbb20ed5b66
Ethan Arteta <129544035+ethanarteta@users.noreply.github.com>
2023-10-14 06:38:16 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 python3.7 -m venv test
 source test/bin/activate
-git clone https://github.com/kura-labs-org/c4_deployment-5.git
+git clone https://github.com/ethanarteta/C4Deployment5.git
 cd c4_deployment-5
 pip install -r requirements.txt
 pip install gunicorn
